1095878a730347c30b87781b76a2c023bbfef067
MINHYUK PARK <79528323+shirohacker@users.noreply.github.com>
2022-09-09 15:06:59 +0900

    35. To-do 생성 페이지 만들기

---
+++ 35. To-do 생성 페이지 만들기/src/pages/todos/create/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Create Todo</h1>
+  <TodoForm />
+</template>
+
+<script>
+import TodoForm from '@/components/TodoForm'
+
+export default {
+  components: {
+    TodoForm,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
